a00e34cddd4a30978fad9931cef74851eacbf32a
audreybsnrd <39850754+audreybsnrd@users.noreply.github.com>
2018-06-08 11:27:46 +0200

    Update formulaire_ajout_nounou.php

--- lo07_projet/formulaire_ajout_nounou.php
+++ lo07_projet/formulaire_ajout_nounou.php
@@ -6,7 +6,21 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <style>
-             div {
+            input[type=submit] {
+                width: 100%;
+                background-color: #4CAF50;
+                color: white;
+                padding: 14px 20px;
+                margin: 8px 0;
+                border: none;
+                border-radius: 4px;
+                cursor: pointer;
+            }
+
+            input[type=submit]:hover {
+                background-color: #45a049;
+            } 
+            div {
                 border-radius: 5px;
                 background-color: #f2f2f2;
                 padding: 20px;
