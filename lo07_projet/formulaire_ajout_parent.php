0bb3944f928dce9376addeab113ca224821d571b
audreybsnrd <39850754+audreybsnrd@users.noreply.github.com>
2018-06-09 13:19:30 +0200

    Update formulaire_ajout_parent.php

--- lo07_projet/formulaire_ajout_parent.php
+++ lo07_projet/formulaire_ajout_parent.php
@@ -7,7 +7,7 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Inscription parents | Les Bambins</title>
     </head>
     <body>
         Bonjour parents
