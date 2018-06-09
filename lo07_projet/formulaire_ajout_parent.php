0c0ba2e5d81465c6e18829b4e2e7e5ab81d3475c
audreybsnrd <39850754+audreybsnrd@users.noreply.github.com>
2018-06-09 13:44:19 +0200

    Update formulaire_ajout_parent.php

--- lo07_projet/formulaire_ajout_parent.php
+++ lo07_projet/formulaire_ajout_parent.php
@@ -10,7 +10,46 @@ and open the template in the editor.
         <title>Inscription parents | Les Bambins</title>
     </head>
     <body>
-        Bonjour parents
+        <form name="ajout_parent" method='post' action='apres_soumission_parent.php' >
+            
+            <p>
+                <label for="prenom">PrÃ©nom</label> : <input type="text" name="prenom" id="prenom" />
+            </p>
+            
+            <p>
+                <label for="nom">Nom</label> : <input type="text" name="nom" id="nom"  />
+            </p>
+            
+            <p>
+                <label for="ville">Ville</label> : <input type="text" name="ville" id="ville" />
+            </p>
+            
+            <p>
+                <label for="mail">Mail</label> : <input type="email" name="mail" id="mail"  />
+            </p>
+            
+            <p>
+                <label for="tel">TÃ©lÃ©phone</label> : <input type="tel" name="tel" id="tel" />
+            </p>
+            
+            <p>
+                <label for="age">Age</label> : <input type="number" name="age" id="age" />
+            </p>
+            
+            <p>
+                <label for="langue"></label>Langue  : <input type="text" name="langue" id="langue"/> <input type="button" onclick="ajout(element);" value="+"/>
+            </p>
+            
+            <p>
+                <label for="presentation">Une phrase de prÃ©sentation</label> : <textarea name="presentation" id="presentation" ></textarea>
+            </p>
+            
+            <p>
+                <input type="submit" value="Envoyer" />
+            </p>   
+              
+                        
+        </form> 
         
         
     </body>
